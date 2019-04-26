0c3479dd6e66abffe54bc69d3bd999f527d5ca63
Charlie <charleschengcy94@gmail.com>
2019-04-26 02:04:52 +0000

    配置AWS-S3

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,11 +57,20 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+            'key' => env('S3_KEY'),
+            'secret' => env('S3_SECRET'),
+            'region' => env('us-east-2'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
+
+        ],
+
+        's3' => [
+            'driver' => 's3',
+            'key' => env('S3_KEY'),
+            'secret' => env('S3_SECRET'),
+            'region' => env('us-east-2'),
+            'bucket' => 'images.wetube.com',
+
         ],
 
     ],
